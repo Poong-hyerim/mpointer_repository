52e7d1968032f5abe9f23b21b4bf5b8a78bbd535
Poonghyerim <poonghrdd@gmil.com>
2021-11-16 13:35:39 +0900

    HW#10_M

--- mpointer.c
+++ mpointer.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 void func1(int* p);
 void func2(int** p);
@@ -14,9 +14,9 @@ int main() {
 	dp = &p;
 	tp = &dp;
 
-	printf("HELLO! :>");
+	printf("HELLO! :>\n");
 
-	unc1(&i);
+	func1(&i);
 	printf("i = %d\n", i);
 	
 	func2(&p);
